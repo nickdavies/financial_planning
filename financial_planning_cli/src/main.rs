5dcee69c88b8d92de63f8f5fadbd310020844e39
Nick Davies <git@nicolasdavies.com.au>
2021-11-25 11:46:37 -0800

    Add basics for outputting a few different summaries

--- financial_planning_cli/src/main.rs
+++ financial_planning_cli/src/main.rs
@@ -7,24 +7,58 @@ mod input;
 mod output;
 
 #[derive(Debug, StructOpt)]
+struct RunOpts {
+    /// How to display the output of the model
+    #[structopt(subcommand)]
+    output_format: output::OutputType,
+}
+
+#[derive(Debug, StructOpt)]
+struct PrintOpts {}
+
+#[derive(Debug, StructOpt)]
+enum Cmd {
+    /// Run a model and generate the output
+    Run(RunOpts),
+    /// Print the loaded/configured model but don't run it
+    Print,
+}
+
+#[derive(Debug, StructOpt)]
 #[structopt(name = "example", about = "An example of StructOpt usage.")]
 struct Opts {
     /// The path to your top level plan file
     #[structopt(parse(from_os_str))]
     plan_file: PathBuf,
+
+    #[structopt(subcommand)]
+    cmd: Cmd,
 }
 
 fn main() -> Result<()> {
     let opt = Opts::from_args();
 
     let config = input::read_configs(&opt.plan_file).context("Failed to load configs")?;
-    println!("{:#?}", config);
 
-    let (range, mut model) = config
-        .build_model()
-        .context("Failed to build model from configs")?;
-    println!("{:#?}", model);
-    let out = model.run(range);
-    println!("{:#?}", out);
-    Ok(())
+    match opt.cmd {
+        Cmd::Run(cmd_opts) => {
+            let (range, mut model) = config
+                .build_model()
+                .context("Failed to build model from configs")?;
+            let out = model.run(range);
+            cmd_opts
+                .output_format
+                .output(out)
+                .context("failed to display model output")
+        }
+        Cmd::Print => {
+            println!("{:#?}", config);
+            let (range, model) = config
+                .build_model()
+                .context("Failed to build model from configs")?;
+            println!("{:#?}", model);
+            println!("{:#?}", range);
+            Ok(())
+        }
+    }
 }
--- financial_planning_cli/src/output.rs
+++ financial_planning_cli/src/output.rs
@@ -1,1 +1,77 @@
+use std::collections::BTreeSet;
 
+use anyhow::{Context, Result};
+use structopt::StructOpt;
+
+use financial_planning_lib::model::{CategoriesSnapshot, ModelReport};
+
+#[derive(Debug, StructOpt)]
+pub enum OutputType {
+    /// Debug print every detail you have
+    Debug,
+    /// Only print out the final state
+    EndOnly,
+    /// Print out a summary for every simulated year
+    Yearly {
+        #[structopt(long)]
+        include_tax: bool,
+    },
+}
+
+impl OutputType {
+    pub fn output(&self, report: ModelReport) -> Result<()> {
+        match self {
+            Self::Debug => {
+                println!("{:#?}", report);
+            }
+            Self::EndOnly => {
+                Self::print_category_changes(&report.start_values, &report.end_values)
+                    .context("failed to merge categories, this is a bug!")?;
+            }
+            Self::Yearly { include_tax } => {
+                for (year, yearly_report) in report.years {
+                    println!("# {} category summary", year.0);
+                    Self::print_category_changes(
+                        &yearly_report.start_values,
+                        &yearly_report.end_values,
+                    )
+                    .context("failed to merge categories, this is a bug!")?;
+                    println!("");
+
+                    if *include_tax {
+                        println!("# {} tax summary:", year.0);
+                        println!("Change in wealth: {}", yearly_report.tax_summary.net_amount);
+                        println!(
+                            "taxable income: {}",
+                            yearly_report.tax_summary.taxable_income
+                        );
+                        println!("tax withheld: {}", yearly_report.tax_summary.tax_withheld);
+                        println!("tax owed: {}", yearly_report.tax_adjustment.owed);
+                        println!("tax delta: {}", yearly_report.tax_adjustment.delta);
+                        println!("tax rate: {}", yearly_report.tax_adjustment.effective_rate);
+                        println!("");
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn print_category_changes(start: &CategoriesSnapshot, end: &CategoriesSnapshot) -> Result<()> {
+        let mut keys: BTreeSet<_> = start.keys().collect();
+        keys.extend(end.keys());
+
+        for key in keys {
+            let start_value = start
+                .get(&key)
+                .context(format!("Provided start snapshot doesn't contain {:?}", key))?;
+
+            let end_value = end
+                .get(&key)
+                .context(format!("Provided end snapshot doesn't contain {:?}", key))?;
+
+            println!("{} = {} => {}", key.0, start_value, end_value);
+        }
+        Ok(())
+    }
+}
