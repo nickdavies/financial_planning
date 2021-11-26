b975400bcd95b3c4456111ae9283971d2c91600c
Nick Davies <git@nicolasdavies.com.au>
2021-11-26 15:11:42 -0800

    Add monthly summary output

--- financial_planning_cli/src/output.rs
+++ financial_planning_cli/src/output.rs
@@ -3,7 +3,8 @@ use std::collections::BTreeSet;
 use anyhow::{Context, Result};
 use structopt::StructOpt;
 
-use financial_planning_lib::model::{CategoriesSnapshot, ModelReport};
+use financial_planning_lib::model::{CategoriesSnapshot, ModelReport, YearlyReport};
+use financial_planning_lib::time::Year;
 
 #[derive(Debug, StructOpt)]
 pub enum OutputType {
@@ -15,6 +16,14 @@ pub enum OutputType {
     Yearly {
         #[structopt(long)]
         include_tax: bool,
+    },
+    /// Print out a summary for each simulated month
+    Monthly {
+        #[structopt(long)]
+        include_tax: bool,
+
+        #[structopt(long)]
+        include_flows: bool,
     },
 }
 
@@ -30,27 +39,48 @@ impl OutputType {
             }
             Self::Yearly { include_tax } => {
                 for (year, yearly_report) in report.years {
-                    println!("# {} category summary", year.0);
-                    Self::print_category_changes(
-                        &yearly_report.start_values,
-                        &yearly_report.end_values,
-                    )
-                    .context("failed to merge categories, this is a bug!")?;
-                    println!("");
-
-                    if *include_tax {
-                        println!("# {} tax summary:", year.0);
-                        println!("Change in wealth: {}", yearly_report.tax_summary.net_amount);
-                        println!(
-                            "taxable income: {}",
-                            yearly_report.tax_summary.taxable_income
-                        );
-                        println!("tax withheld: {}", yearly_report.tax_summary.tax_withheld);
-                        println!("tax owed: {}", yearly_report.tax_adjustment.owed);
-                        println!("tax delta: {}", yearly_report.tax_adjustment.delta);
-                        println!("tax rate: {}", yearly_report.tax_adjustment.effective_rate);
-                        println!("");
+                    Self::print_yearly_summaries(year, &yearly_report, *include_tax)?;
+                }
+            }
+            Self::Monthly {
+                include_tax,
+                include_flows,
+            } => {
+                for (year, yearly_report) in report.years {
+                    Self::print_yearly_summaries(year, &yearly_report, *include_tax)?;
+                    println!("## Monthly breakdown for {}", year.0);
+                    for month in year.months() {
+                        for (category, monthly_reports) in yearly_report.category_summary.iter() {
+                            if let Some(monthly_report) = monthly_reports.get(&month.month) {
+                                println!(
+                                    "  {:?} {} = {} => {}",
+                                    month.month,
+                                    category.0,
+                                    monthly_report.start_value,
+                                    monthly_report.end_value
+                                );
+                                if *include_flows {
+                                    for (flow, tx) in &monthly_report.transactions {
+                                        println!(
+                                            "    {}: {}{}",
+                                            flow.0,
+                                            tx.amount,
+                                            if *include_tax {
+                                                format!(
+                                                    " ({} tax withheld)",
+                                                    tx.tax_tx.tax_withheld
+                                                )
+                                            } else {
+                                                "".to_string()
+                                            }
+                                        );
+                                    }
+                                    println!("");
+                                }
+                            }
+                        }
                     }
+                    println!("");
                 }
             }
         }
@@ -70,7 +100,39 @@ impl OutputType {
                 .get(&key)
                 .context(format!("Provided end snapshot doesn't contain {:?}", key))?;
 
-            println!("{} = {} => {}", key.0, start_value, end_value);
+            println!("  {} = {} => {}", key.0, start_value, end_value);
+        }
+        Ok(())
+    }
+
+    fn print_yearly_summaries(
+        year: Year,
+        yearly_report: &YearlyReport,
+        include_tax: bool,
+    ) -> Result<()> {
+        println!("# {} yearly category summary", year.0);
+        Self::print_category_changes(&yearly_report.start_values, &yearly_report.end_values)
+            .context("failed to merge categories, this is a bug!")?;
+        println!("");
+
+        if include_tax {
+            println!("# {} yearly tax summary:", year.0);
+            println!(
+                "  Change in wealth: {}",
+                yearly_report.tax_summary.net_amount
+            );
+            println!(
+                "  taxable income: {}",
+                yearly_report.tax_summary.taxable_income
+            );
+            println!("  tax withheld: {}", yearly_report.tax_summary.tax_withheld);
+            println!("  tax owed: {}", yearly_report.tax_adjustment.owed);
+            println!("  tax delta: {}", yearly_report.tax_adjustment.delta);
+            println!(
+                "  tax rate: {}",
+                yearly_report.tax_adjustment.effective_rate
+            );
+            println!("");
         }
         Ok(())
     }
